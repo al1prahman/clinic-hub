744051624d1ab0592ecc6a1d7ae1b535c087c718
Alip Rahman <al1prahman@users.noreply.github.com>
2026-09-07 20:47:32 +0700

    clean up user model field definitions and improve password hashing

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
 const bcrypt = require('bcryptjs');
 
@@ -7,39 +7,34 @@ const User = sequelize.define('User', {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
-    field: 'id',
   },
   username: {
     type: DataTypes.STRING(50),
     allowNull: false,
     unique: true,
-    field: 'username',
   },
   email: {
     type: DataTypes.STRING(100),
     allowNull: false,
     unique: true,
-    field: 'email',
   },
   password: {
     type: DataTypes.STRING(255),
     allowNull: false,
-    field: 'password',
   },
   role: {
     type: DataTypes.ENUM('admin', 'dokter', 'petugas_pendaftaran'),
     allowNull: false,
     defaultValue: 'petugas_pendaftaran',
-    field: 'role',
   },
-  full_name: {
+  fullName: {
     type: DataTypes.STRING(100),
     allowNull: false,
     field: 'full_name',
   },
-  is_active: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true,
+  isActive: {
+    type: DataTypes.TINYINT(1),
+    defaultValue: 1,
     field: 'is_active',
   },
 }, {
@@ -48,20 +43,21 @@ const User = sequelize.define('User', {
   hooks: {
     beforeCreate: async (user) => {
       if (user.password) {
-        user.password = await bcrypt.hash(user.password, 10);
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
       }
     },
     beforeUpdate: async (user) => {
       if (user.changed('password')) {
-        user.password = await bcrypt.hash(user.password, 10);
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
       }
     },
   },
 });
 
-// Instance method to compare password
-User.prototype.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+User.prototype.validPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = User;
